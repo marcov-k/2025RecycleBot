064a23dcc50756d0603d0f505ae2c717757afd52
Robby McMurry <rrmcmurry@gmail.com>
2025-04-23 13:09:41 -0400

    Wrote a muppet subsystem

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,8 +6,8 @@ package frc.robot;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.subsystems.DriveSubsystem;
-import frc.robot.subsystems.ElevatorSubsystem;
+import frc.robot.subsystems.*;
+
 
 
 
@@ -28,6 +28,9 @@ public class Robot extends TimedRobot {
   // The elevator
   private final ElevatorSubsystem elevator = new ElevatorSubsystem();
   
+  // The muppet
+  private final MuppetSubsystem monkey = new MuppetSubsystem();
+
 
   @Override
   public void robotInit() {
@@ -67,12 +70,15 @@ public class Robot extends TimedRobot {
 
     // Bumpers raise or lower the elevator
     if (controller.getRightBumperButton()) {
+      monkey.elevatorstick(0.6);
       elevator.raise();
     }
     else if (controller.getLeftBumperButton()) {
+      monkey.elevatorstick(0.4);
       elevator.lower();      
     }
     else {
+      monkey.elevatorstick(0.5);
       elevator.stop();
     }
     
@@ -80,6 +86,8 @@ public class Robot extends TimedRobot {
     forward = MathUtil.applyDeadband(-controller.getLeftY()*.2, 0.02) * elevator.elevatorspeedlimiter;
     strafe = MathUtil.applyDeadband(controller.getLeftX()*.2, 0.02) * elevator.elevatorspeedlimiter;
     rotate = MathUtil.applyDeadband(controller.getRightX()*.2, 0.02);
+
+    monkey.drivestick(forward);
 
     // Send controller values to swerve drive
     swerveDrive.drive(forward, strafe, rotate, fieldRelative);
